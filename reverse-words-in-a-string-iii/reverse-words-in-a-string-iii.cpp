dbd0013d965138593a044103e939e7693802d829
Palak <64108970+Palak791@users.noreply.github.com>
2021-10-15 14:40:53 +0530

    Time: 12 ms (94.06%), Space: 9.7 MB (74.72%) - LeetHub

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     string reverseWords(string s) {
-       int i = 0;
-       int j = 1;
-       while(i < s.length()){
-           while(s[j] != 32){
-               j++;
-               if(j == s.length()) break;
-           }
-           reverse(s.begin () + i ,s.begin()+ j);
-           i = j;
-           if(j != s.length()-1)j++;
-           i++;
-          // if(i == s.length()-1 )i++;
-       }
+        int j = 0;
+      for(int i = 0;i <= s.length();i++){
+          if(i == s.length() || s[i] == ' '){
+              reverse(&s[j], &s[i]);// reverse function stl
+              j = i+1;
+          }
+      }
         return s;
     }
 };
