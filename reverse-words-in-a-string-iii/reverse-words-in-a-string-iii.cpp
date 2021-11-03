3954fa97cbfb54354cecdca920ec16aac1d04217
Palak <64108970+Palak791@users.noreply.github.com>
2021-11-03 09:43:27 +0530

    Time: 16 ms (73.52%), Space: 9.7 MB (75.47%) - LeetHub

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string reverseWords(string s) {
         int j = 0;
-      for(int i = 0;i <= s.length();i++){
-          if(i == s.length() || s[i] == ' '){
-              reverse(&s[j], &s[i]);// reverse function stl
-              j = i+1;
-          }
-      }
+        for(int i = 0;i <= s.length();i++){
+            if(s[i] == ' ' || i == s.length()) {
+            reverse(&s[j],&s[i]);//REV FUNC IN STL
+            j = i+1;
+            }
+        }
         return s;
     }
 };
